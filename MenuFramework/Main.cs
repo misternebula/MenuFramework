a98ee8376529f2ff2ac6569e6a48e80141af9ca9
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-05-28 20:34:25 +0100

    Fix blocker issue with pause menu lists

--- MenuFramework/Main.cs
+++ MenuFramework/Main.cs
@@ -47,15 +47,6 @@ namespace MenuFramework
 			}
 		}
 
-		private void Update()
-		{
-			if (LoadManager.GetCurrentScene() == OWScene.SolarSystem || LoadManager.GetCurrentScene() == OWScene.EyeOfTheUniverse)
-			{
-				Helper.Console.WriteLine($"{Resources.FindObjectsOfTypeAll<PauseUI>().First().GetValue<Image>("_backdrop").enabled}, {Resources.FindObjectsOfTypeAll<PauseUI>().First().GetComponent<Canvas>().sortingOrder}");
-				Helper.Console.WriteLine($"PauseMenuCanvas sortorder : {GameObject.Find("PauseMenuCanvas").GetComponent<Canvas>().sortingOrder}");
-			}
-		}
-
 		private void SetUpButtonPrefab()
 		{
 			ButtonPrefab = MenuBundle.LoadAsset<GameObject>("assets/button-custombutton.prefab");
@@ -106,12 +97,15 @@ namespace MenuFramework
 			PauseListPrefab = MenuBundle.LoadAsset<GameObject>("assets/custompausemenuitems.prefab");
 			PauseListPrefab.SetActive(false);
 
+			var canvas = PauseListPrefab.GetComponent<Canvas>();
+			if (canvas != null)
+			{
+				Destroy(canvas);
+			}
+
 			var menu = PauseListPrefab.AddComponent<Menu>();
 			menu.SetValue("_menuActivationRoot", PauseListPrefab);
 			PauseListPrefab.AddComponent<MenuCancelAction>();
-
-			var blocker = PauseListPrefab.transform.GetChild(0);
-			Main.Helper.Console.WriteLine($"{blocker.GetComponent<Canvas>().sortingOrder}");
 
 			var labelText = PauseListPrefab.transform.GetChild(1).GetChild(0).GetChild(0);
 			var styleApplier = labelText.gameObject.AddComponent<TextStyleApplier>();
