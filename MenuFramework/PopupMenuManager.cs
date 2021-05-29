2618f236af2d4bca1e096a3c6486a31a2f488c05
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2021-05-29 15:29:43 +0100

    fix two-input text being changed (and blockers not appearing)

--- MenuFramework/PopupMenuManager.cs
+++ MenuFramework/PopupMenuManager.cs
@@ -14,19 +14,35 @@ namespace MenuFramework
 		{
 			Instance = this;
 
-			_inputPopupBase = Instantiate(Resources.FindObjectsOfTypeAll<PopupMenu>().First(x => x.name == "InputField-Popup" && x.transform.parent.name == "PopupCanvas").gameObject);
+			foreach (var item in Resources.FindObjectsOfTypeAll<PopupMenu>())
+			{
+				Main.Helper.Console.WriteLine($"- {item.name} parent:{item.transform.parent.name} grandparent:{item.transform.parent.parent.name}");
+			}
+			_inputPopupBase = Instantiate(Resources.FindObjectsOfTypeAll<PopupMenu>().First(x => x.name == "InputField-Popup" && x.transform.parent.name == "PopupCanvas" && x.transform.parent.parent.name == "TitleMenu").gameObject);
 			DontDestroyOnLoad(_inputPopupBase);
 			_inputPopupBase.SetActive(false);
 
-			_twoChoicePopupBase = Instantiate(Resources.FindObjectsOfTypeAll<PopupMenu>().First(x => x.name == "TwoButton-Popup" && x.transform.parent.name == "PopupCanvas").gameObject);
+			_twoChoicePopupBase = Instantiate(Resources.FindObjectsOfTypeAll<PopupMenu>().First(x => x.name == "TwoButton-Popup" && x.transform.parent.name == "PopupCanvas" && x.transform.parent.parent.name == "TitleMenu").gameObject);
 			DontDestroyOnLoad(_twoChoicePopupBase);
 			_twoChoicePopupBase.SetActive(false);
 		}
 
 		public PopupMenu CreateTwoChoicePopup(string message, string confirmText, string cancelText)
 		{
+			Main.Helper.Console.WriteLine($"Create two-choice, message:{message}, confirm:{confirmText}, cancel:{cancelText}");
 			var newPopup = Instantiate(_twoChoicePopupBase);
-			newPopup.transform.parent = GameObject.Find("PopupCanvas").transform;
+
+			switch (LoadManager.GetCurrentScene())
+			{
+				case OWScene.TitleScreen:
+					newPopup.transform.parent = GameObject.Find("/TitleMenu/PopupCanvas").transform;
+					break;
+				case OWScene.SolarSystem:
+				case OWScene.EyeOfTheUniverse:
+					newPopup.transform.parent = GameObject.Find("/PauseMenu/PopupCanvas").transform;
+					break;
+			}
+			
 			newPopup.transform.localPosition = Vector3.zero;
 			newPopup.transform.localScale = Vector3.one;
 			newPopup.GetComponentsInChildren<LocalizedText>().ToList().ForEach(x => Destroy(x));
@@ -38,8 +54,20 @@ namespace MenuFramework
 
 		public PopupInputMenu CreateInputFieldPopup(string message, string placeholderMessage, string confirmText, string cancelText)
 		{
+			Main.Helper.Console.WriteLine($"Create input field, message:{message}, placeholder:{placeholderMessage}, confirm:{confirmText}, cancel:{cancelText}");
 			var newPopup = Instantiate(_inputPopupBase);
-			newPopup.transform.parent = GameObject.Find("PopupCanvas").transform;
+
+			switch (LoadManager.GetCurrentScene())
+			{
+				case OWScene.TitleScreen:
+					newPopup.transform.parent = GameObject.Find("/TitleMenu/PopupCanvas").transform;
+					break;
+				case OWScene.SolarSystem:
+				case OWScene.EyeOfTheUniverse:
+					newPopup.transform.parent = GameObject.Find("/PauseMenu/PopupCanvas").transform;
+					break;
+			}
+
 			newPopup.transform.localPosition = Vector3.zero;
 			newPopup.transform.localScale = Vector3.one;
 			newPopup.GetComponentsInChildren<LocalizedText>().ToList().ForEach(x => Destroy(x));
